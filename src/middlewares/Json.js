69ec054bc157bde424366edafa093e7098c35818
Arthur Silva <arthur.santos.off@gmail.com>
2024-04-19 02:23:22 -0300

    feat: create a JSON middleware

---
+++ src/middlewares/Json.js
@@ -0,0 +1,16 @@
+export async function Json(req, res) {
+  const buffers = [];
+
+  // if was a stream await for complete
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
+  res.setHeader("Content-type", "application/json");
+}
--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import http from "node:http";
+import { routes } from "./routes";
+import { Json } from "./middlewares/Json";
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200).end();
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  await Json(req, res);
+
 });
 
 server.listen(3333);
